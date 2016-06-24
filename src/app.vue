ec9a79345c1a790ffe0b572da563d62949688060
edgar.schnueriger <edgar.schnueriger@ricardo.ch>
2016-06-24 09:43:46 +0200

    added footer and header styles

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,15 @@
 <template>
   <app-header></app-header>
-  <div id="content">
+  <div id="content" class="row">
     <!-- // put your article-component here -->
+    <h1>Ready for your article component</h1>
   </div>
   <app-footer></app-footer>
 </template>
 
 <script>
-  var header = require('component~header'),
+  var layout = require('page~global'),
+      header = require('component~header'),
       footer = require('component~footer');
 
   module.exports = {
